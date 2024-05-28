5e7268e4e2133cdda1f36d0a569be9dbaeb61ec7
mikael-espinola <mikaelespinolaa@gmail.com>
2024-05-28 19:58:32 -0300

    FIX: wa linking

--- src/components/whatsappButton/WAButton.tsx
+++ src/components/whatsappButton/WAButton.tsx
@@ -6,7 +6,7 @@ import { FaWhatsapp } from "react-icons/fa";
 const WAButton = () => {
   return (
     <div className="flex items-center justify-center fixed bottom-3 right-2 rounded-full p-[0.5em] bg-green-50 ">
-      <Link href="https://wa.me/5584222600500">
+      <Link href="https://wa.me/558422260500">
         <span className="text-green-400 hover:text-green-600 text-5xl">
           <FaWhatsapp />
         </span>
